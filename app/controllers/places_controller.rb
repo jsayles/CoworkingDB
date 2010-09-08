61c2a2c3818708783ab1b17840f7c4a7f20557bc
Matt London <matt@londoncontrols.com>
2010-09-08 14:57:54 -0700

    Added views for places.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,11 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.order("name ASC")
+    @places = Place.where("parent_id IS NULL OR parent_id = ''").order("name ASC")
+  end
+  
+  def show
+    @place = Place.find(params[:id])
   end
 
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,43 @@
 class Place < ActiveRecord::Base
 
   validates_presence_of :name
+  validates_uniqueness_of :name
+  has_many :spaces
+  acts_as_tree
+
+  def name_with_space_count
+    spaces.any? ? "#{name} (#{spaces.count})" : name
+  end
+  
+  def lineage
+    list = []
+    if parent
+      list << parent
+      list << parent.lineage
+    end
+    list.flatten
+  end
+  
+  def descendants
+    list = []
+    if children.any?
+      children.each do |child|
+        list << child
+        list << child.descendants
+      end
+    end
+    list.flatten
+  end
+  
+  def all_spaces
+    list = []
+    list << spaces
+    if descendants.any?
+      descendants.each do |descendant|
+        list << descendant.spaces
+      end
+    end
+    list.flatten
+  end
 
 end
