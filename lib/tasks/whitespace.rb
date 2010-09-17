237f77072a52564241b29f63cdf4f2aae4306e04
Kevin Moore <kevin@thinkpixellab.com>
2010-09-16 21:20:38 -0700

    Added 'pre' tasks

---
+++ lib/tasks/whitespace.rb
@@ -0,0 +1,33 @@
+def clean_whitespace(file_name)
+  lines = []
+  File.open(file_name) do |file|
+    file.each_line do |line|
+      lines << line
+    end
+  end
+
+  clean = true
+  lines.each_index do |index|
+    line = lines[index]
+    new_line = line.gsub(/[ \t]+$/, "") #match one or more spaces/tabs at the end of the line -> nothing
+    new_line = new_line.gsub(/\t/, "  ") #match all tabs and replace with two spaces
+
+    if(new_line != line)
+
+      if(clean)
+        puts "Cleaning '#{file_name}'"
+        clean = false
+      end
+
+      puts "Line #{index+1}"
+
+      lines[index] = new_line
+    end
+  end
+
+  if(!clean)
+    File.open(file_name, 'w') do |file|
+      lines.each { |line| file.write(line) }
+    end
+  end
+end
