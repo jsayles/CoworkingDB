6984bab729da61c475543940d85c8bbbc086e68b
Kevin Moore <kevin@thinkpixellab.com>
2010-09-17 08:38:36 -0700

    Show lat/long from the database

--- app/models/space.rb
+++ app/models/space.rb
@@ -6,7 +6,13 @@ class Space < ActiveRecord::Base
 
   accepts_nested_attributes_for :place, :reject_if => lambda { |a| a[:name].blank? }
 
-
+  def coordinates
+    if lat && long
+      {:lat => lat, :long => long}
+    else
+      nil
+    end
+  end
 
   def details?
     url? || twitter_url?
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -16,6 +16,35 @@ function initMap() {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map = new google.maps.Map(document.getElementById("map"), myOptions);
+
+  if (spaceData) {
+    addSpaces(map, spaceData);
+  }
+}
+
+
+
+function addSpaces(map, spacesArray) {
+  for (var i in spacesArray) {
+    var space = spacesArray[i]['space'];
+    var myLatlng = new google.maps.LatLng(space['lat'], space['long']);
+
+    var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: space['name']
+    });
+  }
+}
+
+
+
+function placeMarker(map, location) {
+  var clickedLocation = new google.maps.LatLng(location);
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
 }
 
 $(document).ready(function() {
