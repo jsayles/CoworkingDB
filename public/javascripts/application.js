69a1a8e19b5039e6f6ae53cb550601c43327c824
Kevin Moore <kevin@thinkpixellab.com>
2010-09-17 07:59:13 -0700

    Moving to google's dynamic map API

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,24 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-$(document).ready(function() {
-  $('#footnotes_debug').each(function() {
+function footnotes_hide() {
+  $('#footnotes_debug').hide();
+  var toggle = document.createElement('input');
+  $(toggle).attr('id', 'footnotes_debug_toggler').attr('type', 'button').attr('value', 'FN').click(function() {
+    $('#footnotes_debug').toggle();
+  }).appendTo($(document.body));
+}
 
-    $('#footnotes_debug').hide();
-    var toggle = document.createElement('input');
-    $(toggle).attr('id', 'footnotes_debug_toggler').attr('type', 'button').attr('value', 'FN').click(function() {
-      $('#footnotes_debug').toggle();
-    }).appendTo($(document.body));
-  });
+
+
+function initMap() {
+  var myLatlng = new google.maps.LatLng(47.616, -122.323);
+  var myOptions = {
+    zoom: 16,
+    center: myLatlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map"), myOptions);
+}
+
+$(document).ready(function() {
+  $('#footnotes_debug').each(footnotes_hide);
+  $('#map').each(initMap);
 });
