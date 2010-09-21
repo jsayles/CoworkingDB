2384cbb7feece85f4929c8629e5ac8181355cccf
Kevin Moore <kevin@thinkpixellab.com>
2010-09-20 17:04:14 -0700

    Fixes to js model. Closes #15.

---
+++ public/javascripts/Coworking.Application.js
@@ -0,0 +1,51 @@
+goog.provide('Coworking.Application');
+
+goog.require('Coworking.Map');
+
+Coworking.Application = function() {
+  if (document.getElementById('footnotes_debug')) {
+    Coworking.Application._initFootnotes();
+  }
+  if (document.getElementById('map')) {
+    this.m_map = new Coworking.Map('map');
+  }
+
+  var doStats = doStats || false;
+  if (doStats) {
+    Coworking.Application._addStats();
+  }
+};
+
+Coworking.Application.prototype.getMap = function() {
+  return this.m_map;
+};
+
+Coworking.Application._initFootnotes = function() {
+  $('#footnotes_debug').hide();
+  var toggle = document.createElement('input');
+  $(toggle).attr('id', 'footnotes_debug_toggler').attr('type', 'button').attr('value', 'FN').click(function() {
+    $('#footnotes_debug').toggle();
+  }).appendTo($(document.body));
+};
+
+Coworking.Application._addStats = function() {
+  Application._addScript("http://www.google-analytics.com/ga.js");
+  Application._addScript("http://www.statcounter.com/counter/counter_xhtml.js");
+};
+
+Coworking.Application._addScript = function(script_uri) {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.async = true;
+  script.src = script_uri;
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(script, s);
+};
+
+var application;
+$(document).ready(function() {
+  application = new Coworking.Application();
+  application.getMap().populateSpaces(viewData.spaceData, true);
+});
+
+var viewData = {};
--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,4 +1,4 @@
-Application.Searcher = function(searchFieldSelector, statusSelector, addressFieldSelector, latFieldSelector, longFieldSelector, map) {
+Coworking.Searcher = function(searchFieldSelector, statusSelector, addressFieldSelector, latFieldSelector, longFieldSelector, map) {
   this.m_inputSelector = searchFieldSelector;
   this.m_latSelector = latFieldSelector;
   this.m_longSelector = longFieldSelector;
@@ -7,25 +7,25 @@ Application.Searcher = function(searchFieldSelector, statusSelector, addressFiel
   this.m_map = map;
 };
 
-Application.Searcher.prototype.update = function() {
+Coworking.Searcher.prototype.update = function() {
   var searchString = $(this.m_inputSelector).val();
   this._searchAddress(searchString, this._success, this._error);
   this._status("Searching...");
 };
 
-Application.Searcher.prototype._searchAddress = function codeAddress(searchString) {
-  if (!Application.Searcher._geocoder) {
-    Application.Searcher._geocoder = new google.maps.Geocoder();
+Coworking.Searcher.prototype._searchAddress = function codeAddress(searchString) {
+  if (!Coworking.Searcher._geocoder) {
+    Coworking.Searcher._geocoder = new google.maps.Geocoder();
   }
 
-  Application.Searcher._geocoder.geocode({
+  Coworking.Searcher._geocoder.geocode({
     'address': searchString
   },
   goog.bind(this._geocodeResult, this)
   );
 };
 
-Application.Searcher.prototype._geocodeResult = function(results, status) {
+Coworking.Searcher.prototype._geocodeResult = function(results, status) {
   if (status != google.maps.GeocoderStatus.OK) {
     this._error("There was an error from the Google Geocoder:" + status);
     return;
@@ -42,7 +42,7 @@ Application.Searcher.prototype._geocodeResult = function(results, status) {
   }
 };
 
-Application.Searcher.prototype._success = function(location, newAdress) {
+Coworking.Searcher.prototype._success = function(location, newAdress) {
   $(this.m_addressSelector).val(newAdress);
   $(this.m_latSelector).val(location.lat());
   $(this.m_longSelector).val(location.lng());
@@ -63,7 +63,7 @@ Application.Searcher.prototype._success = function(location, newAdress) {
   this.m_map.setZoom(16);
 };
 
-Application.Searcher.prototype._error = function(message, opt_bounds) {
+Coworking.Searcher.prototype._error = function(message, opt_bounds) {
   this._clearMarker();
   this._status(message);
   if (opt_bounds) {
@@ -71,7 +71,7 @@ Application.Searcher.prototype._error = function(message, opt_bounds) {
   }
 };
 
-Application.Searcher.prototype._status = function(message) {
+Coworking.Searcher.prototype._status = function(message) {
   if (message) {
     $(this.m_statusSelector).show().text(message);
   } else {
@@ -79,7 +79,7 @@ Application.Searcher.prototype._status = function(message) {
   }
 };
 
-Application.Searcher.prototype._clearMarker = function() {
+Coworking.Searcher.prototype._clearMarker = function() {
   if (this.m_marker) {
     this.m_marker.setMap(null);
     this.m_marker = null;
@@ -88,7 +88,7 @@ Application.Searcher.prototype._clearMarker = function() {
 
 var searcher;
 $(document).ready(function() {
-  searcher = new Application.Searcher('#searchField', '#searchStatus', '#address', '#lat', '#long', application.map);
+  searcher = new Coworking.Searcher('#searchField', '#searchStatus', '#address', '#lat', '#long', application.getMap().getGoogMap());
   searcher.update();
 
   $("#searchField").keyup(function(event) {
