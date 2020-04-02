e4e16a722511dd70c84c036b51eb8ea5f429ca1a
Jacob Sayles <jsayles@gmail.com>
2020-04-02 09:36:33 -0700

    fixed but with robots.txt

--- crdb/urls.py
+++ crdb/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView
 from django.contrib.auth import views as auth_views
+from django.http import HttpResponse, HttpResponseRedirect
+
 
 from crdb import views
 
